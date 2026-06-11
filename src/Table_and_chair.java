c26e431d172c661c931aa430145c1a384c8876a3
elvistenkorang69@gmail.com <otfworkout1@gmail.com>
2026-06-11 12:33:40 +0000

    initial commit

---
+++ src/Table_and_chair.java
@@ -0,0 +1,20 @@
+public class Table_and_chair {
+    public static void main(String[] args) {
+
+
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X X X X X X X X X X X X        X X X X X X X X X X X X X    X X X X X X X X X X X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+    }
+}
